708501a8a3c8ba46b641d8a31eb2ee7ca979eeaa
Nick Vela <nickvela@Nicks-MacBook-Pro.local>
2013-09-12 17:51:48 -0700

    bottles of beer, grandma convo

---
+++ examples.rb
@@ -0,0 +1,13 @@
+puts 'Hello, what\'s your name?'
+name = gets.chomp
+puts 'Hello ' + name + '!'
+
+if name == 'Nick'
+	puts 'What a tight name brah!'
+elsif name == 'Kelly'
+	puts 'That\'s a fine name sweety!'
+elsif name == 'Ben'
+	puts 'That\'s a tubular name buddy!'
+end
+
+
