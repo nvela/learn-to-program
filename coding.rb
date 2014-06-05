d6fda9eb5c4e03901190b79718158198c4e0cca2
Nick Vela <nickvela@Nicks-MacBook-Pro.local>
2014-06-05 13:04:37 -0700

    shoring up learning-loggery directory

--- coding.rb
+++ coding.rb
@@ -1,2 +1,16 @@
-puts 1 + 2
+puts "Would you like to know how may hours are in a year?"
+while true
+	response = gets.chomp
+	if response == "yes".capitalize
+		puts "There are X hours in a year!"
+			break
+	end
+	if response == "no"
+		puts "Well, that's just sad!"
+			break
+	end
+	if response != "yes" or "no"
+		puts "Well? Do you want to know the answer or not?!"
+	end
+end
 
